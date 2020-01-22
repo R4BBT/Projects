b8aafe5480a3f3b8c3b694ee4dda506063ab31e7
R4BBT <41391053+R4BBT@users.noreply.github.com>
2020-01-22 18:12:14 -0500

    Delete twitter_credentials.py

--- twitter_credentials.py
+++
@@ -1,6 +0,0 @@
-# Variables that contain the user credentials to access twitter API
-ACCESS_TOKEN = '960353916352376832-6EtdS8ZxEGVYGbpniJpAYe5CFn8a5hS'
-ACCESS_TOKEN_SECRET = 'VHQfWMbrnhrB5vc3NX0LrWcEhewObtr2vVh1QTU5EgMsL'
-
-CONSUMER_KEY = '6Day3OEYDwa8MSyMWDepC7zKZ'
-CONSUMER_SECRET = '9weDkjuIx70FnkAzC6eRh2awOUqyWpVrlixEECQMYjDPYg0Oz6'
